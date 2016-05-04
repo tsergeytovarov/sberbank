952830560a419c22a1debea6d2d56ddaccf6ecc3
Sergey Popov <tovarov.piter@gmail.com>
2016-05-04 14:54:13 +0300

    финальные изменения

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,7 @@
-'use strict';
+$(document).ready(function(){
+  $(".mask").inputmask("99/99/9999");
+
+  var start = $(".promo--video").height();
+
+  $(".promo--video .container").css("min-height", start + "px");
+});
